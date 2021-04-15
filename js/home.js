c276eb54f3ff010b460effd968bd92de6c2490bf
tayinde <tbiayinde@gmail.com>
2021-04-15 18:46:45 -0400

    minor edit on copy

--- js/home.js
+++ js/home.js
@@ -1,6 +1,6 @@
 $('#contact-section .social-surrounding').eq(0).click(() => {
     navigator.clipboard.writeText("ta334__")
-    $('.copied').eq(0).css('width', '200px')
+    $('.copied').eq(0).css('width', '150px')
     $('.copied').eq(0).css('opacity', '1')
     setTimeout(() => {
         $('.copied').eq(0).css('width', '0px')
@@ -10,7 +10,7 @@ $('#contact-section .social-surrounding').eq(0).click(() => {
 
 $('#contact-section .social-surrounding').eq(1).click(() => {
     navigator.clipboard.writeText("B4B4TUND3#9960")
-    $('.copied').eq(1).css('width', '200px')
+    $('.copied').eq(1).css('width', '150px')
     $('.copied').eq(1).css('opacity', '1')
     setTimeout(() => {
         $('.copied').eq(1).css('width', '0px')
@@ -20,7 +20,7 @@ $('#contact-section .social-surrounding').eq(1).click(() => {
 
 $('#contact-section .social-surrounding').eq(2).click(() => {
     navigator.clipboard.writeText("tbiayinde@gmail.com")
-    $('.copied').eq(2).css('width', '200px')
+    $('.copied').eq(2).css('width', '150px')
     $('.copied').eq(2).css('opacity', '1')
     setTimeout(() => {
         $('.copied').eq(2).css('width', '0px')
@@ -30,7 +30,7 @@ $('#contact-section .social-surrounding').eq(2).click(() => {
 
 $('#contact-section .social-surrounding').eq(3).click(() => {
     navigator.clipboard.writeText("tayinde")
-    $('.copied').eq(3).css('width', '200px')
+    $('.copied').eq(3).css('width', '150px')
     $('.copied').eq(3).css('opacity', '1')
     setTimeout(() => {
         $('.copied').eq(3).css('width', '0px')
@@ -40,7 +40,7 @@ $('#contact-section .social-surrounding').eq(3).click(() => {
 
 $('#contact-section .social-surrounding').eq(4).click(() => {
     navigator.clipboard.writeText("https://stackoverflow.com/users/11930170/t-a")
-    $('.copied').eq(4).css('width', '200px')
+    $('.copied').eq(4).css('width', '150px')
     $('.copied').eq(4).css('opacity', '1')
     setTimeout(() => {
         $('.copied').eq(4).css('width', '0px')
