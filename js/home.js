a28ab48eea7682463abe1940738a4743d780d674
tayinde <tbiayinde@gmail.com>
2021-04-15 15:06:07 -0400

    initial commit

---
+++ js/home.js
@@ -0,0 +1,49 @@
+$('#contact-section .social-surrounding').eq(0).click(() => {
+    navigator.clipboard.writeText("ta334__")
+    $('.copied').eq(0).css('width', '200px')
+    $('.copied').eq(0).css('opacity', '1')
+    setTimeout(() => {
+        $('.copied').eq(0).css('width', '0px')
+        $('.copied').eq(0).css('opacity', '0')
+    }, 2000);
+})
+
+$('#contact-section .social-surrounding').eq(1).click(() => {
+    navigator.clipboard.writeText("B4B4TUND3#9960")
+    $('.copied').eq(1).css('width', '200px')
+    $('.copied').eq(1).css('opacity', '1')
+    setTimeout(() => {
+        $('.copied').eq(1).css('width', '0px')
+        $('.copied').eq(1).css('opacity', '0')
+    }, 2000);
+})
+
+$('#contact-section .social-surrounding').eq(2).click(() => {
+    navigator.clipboard.writeText("tbiayinde@gmail.com")
+    $('.copied').eq(2).css('width', '200px')
+    $('.copied').eq(2).css('opacity', '1')
+    setTimeout(() => {
+        $('.copied').eq(2).css('width', '0px')
+        $('.copied').eq(2).css('opacity', '0')
+    }, 2000);
+})
+
+$('#contact-section .social-surrounding').eq(3).click(() => {
+    navigator.clipboard.writeText("tayinde")
+    $('.copied').eq(3).css('width', '200px')
+    $('.copied').eq(3).css('opacity', '1')
+    setTimeout(() => {
+        $('.copied').eq(3).css('width', '0px')
+        $('.copied').eq(3).css('opacity', '0')
+    }, 2000);
+})
+
+$('#contact-section .social-surrounding').eq(4).click(() => {
+    navigator.clipboard.writeText("https://stackoverflow.com/users/11930170/t-a")
+    $('.copied').eq(4).css('width', '200px')
+    $('.copied').eq(4).css('opacity', '1')
+    setTimeout(() => {
+        $('.copied').eq(4).css('width', '0px')
+        $('.copied').eq(4).css('opacity', '0')
+    }, 2000);
+})
